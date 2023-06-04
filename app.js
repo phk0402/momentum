78476ce1eca10461e8a300e1414604e46a873517
phk0402 <hanafil9742@gmail.com>
2023-06-04 21:59:09 +0900

    css in js

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  h1.style.color = "red";
-}
-
-function handleMouseEnter() {
-  h1.innerText = "mouse is here!"
-}
-
-function handleMouseLeave() {
-  h1.innerText = "mouse is gone"
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI")
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOD")
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato"
+  } else {
+    newColor = "blue"
+  }
+  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
