8ae8138817e2f4a2f5ca2798f82810728a3e79bc
phk0402 <hanafil9742@gmail.com>
2023-06-11 21:26:11 +0900

    background random images

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["0.png", "1.png", "2.png"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage)
