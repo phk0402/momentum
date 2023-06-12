1f40f550e49e31b1c00ada270857ab6ea7a93bcf
phk0402 <hanafil9742@gmail.com>
2023-06-12 22:58:57 +0900

    todo 작업중

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input")
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+
+}
 
 function handleToDoSubmit(evnet) {
   evnet.preventDefault();
   const newToDo = todoInput.value;
   todoInput.value = "";
+  paintToDo(newToDo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit)
