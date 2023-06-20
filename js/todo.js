d56a96172f58a2d431500639bd5c55c85ae394be
phk0402 <hanafil9742@gmail.com>
2023-06-20 22:42:56 +0900

    deleting to do items

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input")
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+   const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo; // span의 텍스트는 handleToDoSubmit에서 온 newTodo 텍스트가 됨
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
