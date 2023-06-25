d2c94c9083ec6a3de2affad97468e99e16bc405f
phk0402 <hanafil9742@gmail.com>
2023-06-25 15:14:22 +0900

    greeting 반복단어 변수처리

--- practice/js/greeting.js
+++ practice/js/greeting.js
@@ -2,27 +2,29 @@ const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector(".greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 function handleLodId(e) {
   e.preventDefault();
-  loginForm.classList.add("hidden");
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
   paintGreeting(username);
 }
 
 function paintGreeting(username) {
   greeting.innerText = username;
-  greeting.classList.remove("hidden");
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
 
 // username이 없을때? 로그인폼 나타남
 // username이 있을때? greeting 인사말 나타남
-
-const savedUsername = localStorage.getItem("username");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null) {
-  loginForm.classList.remove("hidden");
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", handleLodId);
 } else {
   paintGreeting(savedUsername);
